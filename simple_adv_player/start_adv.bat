73d878d828093992305514383b2e9f3c54c38754
Umetana <umetanabe1978jp@gmail.com>
2026-08-08 06:51:16 +0900

    feat: 新しい将来機能メモを追加し、start_adv.batの起動順序を修正

--- simple_adv_player/start_adv.bat
+++ simple_adv_player/start_adv.bat
@@ -1,8 +1,9 @@
 @echo off
 cd /d "%~dp0"
 
-start "" http://127.0.0.1:8770/editor/
 set SIMPLE_ADV_PORT=8770
-node tools\dev-server.mjs
 
-pause
+start "Simple ADV Server" cmd /k "node tools\dev-server.mjs"
+
+timeout /t 1 /nobreak >nul
+start "" http://127.0.0.1:8770/editor/
